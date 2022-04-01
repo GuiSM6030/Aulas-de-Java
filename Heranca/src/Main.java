8fec757c6226a6a91e228124a54640cc68ae09d1
GuiSM6030 <102614840+GuiSM6030@users.noreply.github.com>
2022-04-01 17:02:27 -0300

    Add files via upload
    01/04

--- Heranca/src/Main.java
+++ Heranca/src/Main.java
@@ -1,14 +1,12 @@
 
 public class Main {
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	
+	Carro carro1 = new Carro();
 
-		Carro carro1 = new Carro();
-
-		carro1.ano = 1936;
-		carro1.setMarca("Mercedez");
-		carro1.setModelo("IMC5");
-		carro1.setQuatroPortas(true);
-		
-		carro1.imprimirDados();
-	}
+carro1.setAno() = 1936;
+	carro1.setMarca = "Mercedez";
+			carro1.setModelo = "IMC5";
+			carro1.setQuatroPortas(true);
+			}
 }
