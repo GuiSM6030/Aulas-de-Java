cec89ed84e5f66d63b992bad7bdbbddf26781560
aluno <aluno@DSN1251230716>
2022-04-01 14:13:41 -0300

    Ss

--- Heranca/src/Main.java
+++ Heranca/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		Carro carro1 = new Carro();
 
 		carro1.ano = 1936;
-		carro1.setMarca("Bilubilu");
+		carro1.setMarca("Mercedez");
 		carro1.setModelo("IMC5");
 		carro1.setQuatroPortas(true);
 		
